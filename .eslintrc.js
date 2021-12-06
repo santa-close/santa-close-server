f8805fa7d66c7ab0109439cb328d6e7f9a5c3832
Jake Son <sonjeabin@gmail.com>
2021-12-06 22:47:12 +0900

    chore: change config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,6 @@
+/**
+ * @type {import("eslint").Linter.Config}
+ */
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -20,5 +23,6 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'no-console': 'error'
   },
 };
