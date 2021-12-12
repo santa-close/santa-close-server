255cffe0bae54b5079507ddaa311604984d7451d
Jake Son <sonjeabin@gmail.com>
2021-12-12 11:31:22 +0900

    feat: change schema first to code first

--- src/author/entities/author.entity.ts
+++ src/author/entities/author.entity.ts
@@ -1,1 +1,7 @@
-export class Author {}
+import { ObjectType, Field, Int } from '@nestjs/graphql';
+
+@ObjectType()
+export class Author {
+  @Field(() => Int, { description: 'Example field (placeholder)' })
+  exampleField: number;
+}
