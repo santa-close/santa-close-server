e5357a9b9fec128a0798a787f29af1f9d3943b14
Jake Son <sonjeabin@gmail.com>
2021-12-12 12:06:05 +0900

    feat: add docker compose config

--- src/app.module.ts
+++ src/app.module.ts
@@ -28,7 +28,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       type: 'mysql',
       host: 'localhost',
       port: 3306,
-      username: 'root',
+      username: 'test',
       password: 'test',
       database: 'test',
       entities: [],
