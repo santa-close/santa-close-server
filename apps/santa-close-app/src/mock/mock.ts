ff7f07df60748bcef679aa5da8dafb7973bec5ae
Jake Son <sonjeabin@gmail.com>
2021-12-19 18:52:35 +0900

    feat: update mock

--- apps/santa-close-app/src/mock/mock.ts
+++ apps/santa-close-app/src/mock/mock.ts
@@ -6,11 +6,13 @@ export default {
   String: () => lorem.word(),
   Int: () => datatype.number(),
   Boolean: () => datatype.boolean(),
-  Mountains: () => [
-    mountainMock(),
-    mountainMock(),
-    mountainMock(),
-    mountainMock(),
-  ],
-  Mountain: () => mountainMock(),
+  Query: {
+    mountains: () => [
+      mountainMock(37.378349, 127.016167),
+      mountainMock(37.377906, 127.011916),
+      mountainMock(37.37586, 127.015909),
+      mountainMock(37.379611, 127.022136),
+    ],
+    mountain: () => mountainMock(37.379611, 127.022136),
+  },
 };
--- apps/santa-close-app/src/mock/mountain/mountain.mock.ts
+++ apps/santa-close-app/src/mock/mountain/mountain.mock.ts
@@ -2,14 +2,17 @@ import { Mountain } from '../../app-mountain/object/mountain.object';
 import { datatype, lorem, random } from 'faker';
 import { Coordinate } from '../../app-mountain/object/coordinate.object';
 
-export const mountainMock: () => Mountain = () => {
+export const mountainMock: (latitude: number, longitude: number) => Mountain = (
+  latitude,
+  longitude,
+) => {
   const object = new Mountain();
   object.name = random.word();
   object.address = lorem.sentence();
   object.rate = datatype.float({ max: 5, min: 0, precision: 5 });
   object.coordinates = new Coordinate();
-  object.coordinates.longitude = datatype.float();
-  object.coordinates.latitude = datatype.float();
+  object.coordinates.latitude = latitude;
+  object.coordinates.longitude = longitude;
 
   return object;
 };
