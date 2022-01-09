793583f6b79f10a4fb79b5a5dcb08dad96f4a3f8
Jake Son <sonjeabin@gmail.com>
2022-01-09 22:23:59 +0900

    [#14] 확장함수 대신 toOption 사용

--- app/src/main/kotlin/com/santaclose/app/sample/repository/SampleAppQueryRepositoryImpl.kt
+++ app/src/main/kotlin/com/santaclose/app/sample/repository/SampleAppQueryRepositoryImpl.kt
@@ -1,15 +1,16 @@
 package com.santaclose.app.sample.repository
 
 import arrow.core.Option
+import arrow.core.toOption
 import com.santaclose.lib.entity.sample.QSample.sample
 import com.santaclose.lib.entity.sample.Sample
-import com.santaclose.lib.extension.fetchOption
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport
 
 class SampleAppQueryRepositoryImpl : QuerydslRepositorySupport(Sample::class.java), SampleAppQueryRepository {
     override fun findByPrice(price: Int): Option<Sample> {
         return from(sample)
             .where(sample.price.eq(price))
-            .fetchOption()
+            .fetchOne()
+            .toOption()
     }
 }
