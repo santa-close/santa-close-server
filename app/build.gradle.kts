141ec4617a0208e553814cb8c23bd9b24d99b9ea
Jake Son <sonjeabin@gmail.com>
2022-01-14 23:58:45 +0900

    [#16] kotlin-jdsl 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,6 +10,7 @@ plugins {
 dependencies {
     implementation(project(":lib"))
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("com.linecorp.kotlin-jdsl:spring-data-kotlin-jdsl-starter:1.1.0.RELEASE")
     implementation("com.expediagroup", "graphql-kotlin-spring-server", "5.3.1")
     implementation("com.querydsl:querydsl-jpa:5.0.0")
     kapt("com.querydsl:querydsl-apt:5.0.0:jpa")
