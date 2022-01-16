e0bfaff3848f18ab6ccc55403383d1f2ac962742
Jake Son <sonjeabin@gmail.com>
2022-01-16 12:57:23 +0900

    [#18] schema 업데이트 테스트 추가

--- app/src/test/kotlin/com/santaclose/app/AppApplicationTests.kt
+++ app/src/test/kotlin/com/santaclose/app/AppApplicationTests.kt
@@ -1,12 +1,35 @@
 package com.santaclose.app
 
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.fail
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.reactive.server.WebTestClient
+import java.io.File
 
 @SpringBootTest
-internal class AppApplicationTests {
+@AutoConfigureWebTestClient
+internal class AppApplicationTests(
+    @Autowired
+    private val webTestClient: WebTestClient,
+) {
     @Test
-    fun contextLoads() {
-        assert(true)
+    fun generateSchema() {
+        // when
+        val response = webTestClient
+            .get()
+            .uri("/sdl")
+            .exchange()
+
+        // then
+        response
+            .returnResult(String::class.java)
+            .responseBody.reduce { a, b -> a + "\n" + b }
+            .block()
+            ?.let {
+                File("src/main/resources/graphql/schema.graphql").writeText(it)
+            }
+            ?: fail("failed to get SDL")
     }
 }
