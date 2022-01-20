fdd1cba427527754c11f843bb1d7cdb9ebe885b6
Jake Son <sonjeabin@gmail.com>
2022-01-20 23:52:13 +0900

    [#23] validation 의존성 추가

--- app/src/main/kotlin/com/santaclose/app/sample/resolver/SampleAppQueryResolver.kt
+++ app/src/main/kotlin/com/santaclose/app/sample/resolver/SampleAppQueryResolver.kt
@@ -7,13 +7,16 @@ import com.santaclose.app.sample.resolver.dto.SampleAppItemInput
 import com.santaclose.app.sample.service.SampleAppQueryService
 import com.santaclose.lib.web.error.getOrThrow
 import org.springframework.stereotype.Component
+import org.springframework.validation.annotation.Validated
+import javax.validation.Valid
 
 @Component
+@Validated
 class SampleAppQueryResolver(
     private val sampleAppQueryService: SampleAppQueryService,
 ) : Query {
     @GraphQLDescription("샘플 데이터")
-    fun sample(input: SampleAppItemInput): SampleAppDetail =
+    fun sample(@Valid input: SampleAppItemInput): SampleAppDetail =
         sampleAppQueryService
             .findByPrice(input.price)
             .getOrThrow()
--- app/src/main/kotlin/com/santaclose/app/sample/resolver/dto/SampleAppItemInput.kt
+++ app/src/main/kotlin/com/santaclose/app/sample/resolver/dto/SampleAppItemInput.kt
@@ -1,6 +1,9 @@
 package com.santaclose.app.sample.resolver.dto
 
+import javax.validation.constraints.Positive
+
 data class SampleAppItemInput(
+    @field:Positive
     val price: Int,
     val name: String? = "",
 )
