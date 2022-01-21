40d8bbbaf7549fb87d24463ad02e7d3669d7b5a1
Jake Son <sonjeabin@gmail.com>
2022-01-21 23:44:07 +0900

    [#23] validation 의존성 위치조정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,6 @@ plugins {
 dependencies {
     implementation(project(":lib"))
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("com.linecorp.kotlin-jdsl:spring-data-kotlin-jdsl-starter:1.1.0.RELEASE")
     implementation("com.expediagroup", "graphql-kotlin-spring-server", "5.3.1")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -37,6 +37,7 @@ subprojects {
         implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
         implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
         implementation("io.arrow-kt:arrow-core:1.0.1")
+        implementation("org.springframework.boot:spring-boot-starter-validation")
         testImplementation("org.springframework.boot:spring-boot-starter-test") {
             exclude(module = "mockito-core")
         }
