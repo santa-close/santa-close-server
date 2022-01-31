66b4c96adcb1ffe4325d3f7d99b57b250aafd91e
Charming <loadkrnis@gmail.com>
2022-02-01 00:19:21 +0900

    [#22] Mountain 엔티티 수정 (NotNull)

--- lib/src/main/kotlin/com/santaclose/lib/entity/mountain/Mountain.kt
+++ lib/src/main/kotlin/com/santaclose/lib/entity/mountain/Mountain.kt
@@ -1,15 +1,17 @@
 package com.santaclose.lib.entity.mountain
 
 import com.santaclose.lib.entity.BaseEntity
+import javax.persistence.Column
 import javax.persistence.Entity
 import javax.validation.constraints.NotNull
 
 @Entity
 class Mountain(
-    @NotNull
+    @field:NotNull
     var name: String,
 
-    @NotNull
+    @Column(columnDefinition = "TEXT")
+    @field:NotNull
     var detail: String,
 
     // 위치 id,
