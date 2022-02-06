08b43bda0e469539060c0587eb56dce86aaecf1b
Jake Son <sonjeabin@gmail.com>
2022-02-06 14:39:28 +0900

    [#34] jwt 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,5 +11,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("com.linecorp.kotlin-jdsl:spring-data-kotlin-jdsl-starter:1.1.0.RELEASE")
     implementation("com.expediagroup", "graphql-kotlin-spring-server", "5.3.1")
+    implementation("io.jsonwebtoken:jjwt-api:0.11.2")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.2")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.2")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 }
--- app/src/main/kotlin/com/santaclose/app/auth/context/DataFetchingEnvironmentExtension.kt
+++ app/src/main/kotlin/com/santaclose/app/auth/context/DataFetchingEnvironmentExtension.kt
@@ -1,7 +1,17 @@
 package com.santaclose.app.auth.context
 
 import arrow.core.Option
+import arrow.core.getOrElse
 import com.santaclose.lib.entity.appUser.AppUser
+import com.santaclose.lib.web.error.toGraphQLException
 import graphql.schema.DataFetchingEnvironment
 
-fun DataFetchingEnvironment.user(): Option<AppUser> = this.graphQlContext.get("user")
+fun DataFetchingEnvironment.optionUser(): Option<AppUser> =
+    this.graphQlContext.get("user")
+
+fun DataFetchingEnvironment.user(): AppUser =
+    this.optionUser()
+        .getOrElse { throw Exception("사용자가 존재하지 않습니다").toGraphQLException() }
+
+fun DataFetchingEnvironment.userId(): Long =
+    this.user().id
