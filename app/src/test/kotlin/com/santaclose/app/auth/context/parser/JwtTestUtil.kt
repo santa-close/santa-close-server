f5b058c68ec0583ea1f86b3f8ba252a731899534
Jake Son <sonjeabin@gmail.com>
2022-02-07 19:10:10 +0900

    [#34] 토큰 유효기간 검증 테스트 추가

--- app/src/test/kotlin/com/santaclose/app/auth/context/parser/JwtTestUtil.kt
+++ app/src/test/kotlin/com/santaclose/app/auth/context/parser/JwtTestUtil.kt
@@ -3,7 +3,7 @@ package com.santaclose.app.auth.context.parser
 import com.santaclose.app.auth.context.AppSession
 import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.security.Keys
-import java.util.Date
+import java.util.*
 
 internal class JwtTestUtil {
     companion object {
@@ -11,13 +11,15 @@ internal class JwtTestUtil {
 
         private val key = Keys.hmacShaKeyFor(secret.toByteArray())
 
-        fun genToken(session: AppSession? = null): String =
+        fun genToken(session: AppSession? = null, isExpired: Boolean = false): String =
             Jwts
                 .builder()
                 .apply {
                     session?.let { setClaims(mapOf("id" to "${session.id}", "role" to session.role)) }
                 }
-                .setExpiration(Date().apply { time += 1000 * 60L * 60L * 2L })
+                .setExpiration(
+                    Date().apply { time += 1000 * 60L * 60L * 2L * (if (isExpired) -1 else 1) }
+                )
                 .setIssuedAt(Date())
                 .signWith(key)
                 .compact()
--- app/src/test/kotlin/com/santaclose/app/auth/context/parser/ServerRequestParserImplTest.kt
+++ app/src/test/kotlin/com/santaclose/app/auth/context/parser/ServerRequestParserImplTest.kt
@@ -39,6 +39,22 @@ internal class ServerRequestParserImplTest {
     }
 
     @Test
+    fun `유효기간이 지난 토큰이면 none 을 반환한다`() {
+        // given
+        val user = AppSession(12345, AppUserRole.USER)
+        val request = MockServerRequest
+            .builder()
+            .header("Authorization", "Bearer ${JwtTestUtil.genToken(user, true)}")
+            .build()
+
+        // when
+        val result = parser.parse(request)
+
+        // then
+        result.shouldBeNone()
+    }
+
+    @Test
     fun `유효한 토큰을 읽어 세션을 반환한다`() {
         // given
         val user = AppSession(12345, AppUserRole.USER)
