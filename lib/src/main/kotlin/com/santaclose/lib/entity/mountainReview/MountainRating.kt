5d8a5dc0a5887f749aae3f7d4594853bdc8b0643
Charming <loadkrnis@gmail.com>
2022-02-18 23:06:22 +0900

    [#38] annotation 변경 Size -> Range

--- lib/src/main/kotlin/com/santaclose/lib/entity/mountainReview/MountainRating.kt
+++ lib/src/main/kotlin/com/santaclose/lib/entity/mountainReview/MountainRating.kt
@@ -1,16 +1,16 @@
 package com.santaclose.lib.entity.mountainReview
 
+import org.hibernate.validator.constraints.Range
 import javax.persistence.Embeddable
-import javax.validation.constraints.Size
 
 @Embeddable
 class MountainRating(
-    @field:Size(min = 0, max = 5)
+    @field:Range(min = 0, max = 5)
     var scenery: Byte,
 
-    @field:Size(min = 0, max = 5)
+    @field:Range(min = 0, max = 5)
     var facility: Byte,
 
-    @field:Size(min = 0, max = 5)
+    @field:Range(min = 0, max = 5)
     var traffic: Byte,
 )
