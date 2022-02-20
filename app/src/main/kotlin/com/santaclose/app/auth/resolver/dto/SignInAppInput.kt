7f5bc2a557de7429f2c6b157d97dee63379d25d6
Jake Son <sonjeabin@gmail.com>
2022-02-20 19:43:18 +0900

    [#36] 미사용 validation 제거

--- app/src/main/kotlin/com/santaclose/app/auth/resolver/dto/SignInAppInput.kt
+++ app/src/main/kotlin/com/santaclose/app/auth/resolver/dto/SignInAppInput.kt
@@ -1,9 +1,6 @@
 package com.santaclose.app.auth.resolver.dto
 
-import javax.validation.constraints.NotBlank
-
 data class SignInAppInput(
-    @field:NotBlank
     val code: String,
     val type: SignInType,
 )
