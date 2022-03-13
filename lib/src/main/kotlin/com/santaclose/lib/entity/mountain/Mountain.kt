14872b8ff3882a3d89338147f8b8b5b59d8320f8
Jake Son <sonjeabin@gmail.com>
2022-03-13 19:47:00 +0900

    [#50] 좌표 테이블 연결 제거

--- lib/src/main/kotlin/com/santaclose/lib/entity/mountain/Mountain.kt
+++ lib/src/main/kotlin/com/santaclose/lib/entity/mountain/Mountain.kt
@@ -2,12 +2,10 @@ package com.santaclose.lib.entity.mountain
 
 import com.santaclose.lib.entity.BaseEntity
 import com.santaclose.lib.entity.appUser.AppUser
-import com.santaclose.lib.entity.coordinate.Coordinates
 import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.FetchType.LAZY
 import javax.persistence.ManyToOne
-import javax.persistence.OneToOne
 import javax.validation.constraints.NotNull
 
 @Entity
@@ -16,10 +14,6 @@ class Mountain(
   @Column(columnDefinition = "TEXT") @field:NotNull var detail: String,
 
 // 위치 id,
-
-    @OneToOne(fetch = LAZY)
-    @field:NotNull
-    var coordinates: Coordinates,
 
     @ManyToOne(fetch = LAZY)
     @field:NotNull
--- lib/src/main/kotlin/com/santaclose/lib/entity/restaurant/Restaurant.kt
+++ lib/src/main/kotlin/com/santaclose/lib/entity/restaurant/Restaurant.kt
@@ -3,13 +3,11 @@ package com.santaclose.lib.entity.restaurant
 import com.santaclose.lib.converter.StringListConverter
 import com.santaclose.lib.entity.BaseEntity
 import com.santaclose.lib.entity.appUser.AppUser
-import com.santaclose.lib.entity.coordinate.Coordinates
 import javax.persistence.Column
 import javax.persistence.Convert
 import javax.persistence.Entity
 import javax.persistence.FetchType.LAZY
 import javax.persistence.ManyToOne
-import javax.persistence.OneToOne
 import javax.validation.constraints.NotNull
 
 @Entity
@@ -22,10 +20,6 @@ class Restaurant(
 
     @Convert(converter = StringListConverter::class)
     var images: MutableList<String> = mutableListOf(),
-
-    @OneToOne(fetch = LAZY)
-    @field:NotNull
-    var coordinates: Coordinates,
 
     @ManyToOne(fetch = LAZY)
     @field:NotNull
