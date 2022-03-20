26752a5ca53255b1713d65ba881ece602ae13ff2
Jake Son <sonjeabin@gmail.com>
2022-03-20 14:19:36 +0900

    [#50] gradle 파일 lint 수정

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,24 +1,24 @@
 plugins {
-    id("org.springframework.boot")
-    id("io.spring.dependency-management")
-    kotlin("jvm")
-    kotlin("plugin.spring")
-    kotlin("plugin.jpa")
+  id("org.springframework.boot")
+  id("io.spring.dependency-management")
+  kotlin("jvm")
+  kotlin("plugin.spring")
+  kotlin("plugin.jpa")
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("com.expediagroup", "graphql-kotlin-spring-server", "5.3.2")
-    implementation("org.hibernate:hibernate-spatial")
-    runtimeOnly("com.h2database:h2")
-    runtimeOnly("mysql:mysql-connector-java")
-    testImplementation("com.squareup.okhttp3:okhttp:4.9.3")
-    testImplementation("com.squareup.okhttp3:mockwebserver:4.9.3")
-    testImplementation("org.testcontainers:localstack:1.16.3")
+  implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+  implementation("com.expediagroup", "graphql-kotlin-spring-server", "5.3.2")
+  implementation("org.hibernate:hibernate-spatial")
+  runtimeOnly("com.h2database:h2")
+  runtimeOnly("mysql:mysql-connector-java")
+  testImplementation("com.squareup.okhttp3:okhttp:4.9.3")
+  testImplementation("com.squareup.okhttp3:mockwebserver:4.9.3")
+  testImplementation("org.testcontainers:localstack:1.16.3")
 }
 
 allOpen {
-    annotation("javax.persistence.Entity")
-    annotation("javax.persistence.MappedSuperclass")
-    annotation("javax.persistence.Embeddable")
+  annotation("javax.persistence.Entity")
+  annotation("javax.persistence.MappedSuperclass")
+  annotation("javax.persistence.Embeddable")
 }
