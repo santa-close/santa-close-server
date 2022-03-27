f261452de781084133a9e103fffe7b7c97b0fd7d
Charming <loadkrnis@gmail.com>
2022-03-27 18:56:16 +0900

    [#54] RestaurantReview entity 수정  - images, mountain, appUser 추가

--- lib/src/main/kotlin/com/santaclose/lib/entity/restaurantReview/RestaurantReview.kt
+++ lib/src/main/kotlin/com/santaclose/lib/entity/restaurantReview/RestaurantReview.kt
@@ -1,7 +1,11 @@
 package com.santaclose.lib.entity.restaurantReview
 
+import com.santaclose.lib.converter.StringListConverter
 import com.santaclose.lib.entity.BaseEntity
+import com.santaclose.lib.entity.appUser.AppUser
+import com.santaclose.lib.entity.mountain.Mountain
 import com.santaclose.lib.entity.restaurant.Restaurant
+import javax.persistence.Convert
 import javax.persistence.Embedded
 import javax.persistence.Entity
 import javax.persistence.FetchType.LAZY
@@ -14,5 +18,9 @@ class RestaurantReview(
   var title: String,
   var content: String,
   @Valid @Embedded @field:NotNull var rating: RestaurantRating,
+  @Convert(converter = StringListConverter::class)
+  var images: MutableList<String> = mutableListOf(),
   @ManyToOne(fetch = LAZY) @field:NotNull var restaurant: Restaurant,
+  @ManyToOne(fetch = LAZY) @field:NotNull var mountain: Mountain,
+  @ManyToOne(fetch = LAZY) @field:NotNull var appUser: AppUser,
 ) : BaseEntity()
