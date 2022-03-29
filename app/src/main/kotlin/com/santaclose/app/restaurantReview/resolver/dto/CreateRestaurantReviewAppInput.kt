18203c299d268f3a5c7323c558378d6365d3034c
Charming <loadkrnis@gmail.com>
2022-03-29 22:43:42 +0900

    [#54] 유닛 테스트 수정

--- app/src/main/kotlin/com/santaclose/app/restaurantReview/resolver/dto/CreateRestaurantReviewAppInput.kt
+++ app/src/main/kotlin/com/santaclose/app/restaurantReview/resolver/dto/CreateRestaurantReviewAppInput.kt
@@ -1,6 +1,7 @@
 package com.santaclose.app.restaurantReview.resolver.dto
 
 import com.santaclose.lib.entity.restaurantReview.type.PriceComment
+import javax.validation.Valid
 import javax.validation.constraints.Size
 import org.springframework.format.annotation.NumberFormat
 
@@ -9,7 +10,7 @@ data class CreateRestaurantReviewAppInput(
   @field:NumberFormat(style = NumberFormat.Style.NUMBER) val mountainId: String,
   val title: String,
   val content: String,
-  val rating: RestaurantRatingDto,
+  @field:Valid val rating: RestaurantRatingDto,
   var priceComment: PriceComment,
   var priceAverage: Int,
   @field:Size(max = 10) val images: List<String>,
