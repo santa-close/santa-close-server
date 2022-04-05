da66201322df878b80c57b0f9e1ef2c68ef67aa7
201402407 <banner4@naver.com>
2022-04-06 00:04:13 +0900

    [#62] 맛집 등록 API - Entity mountain 추가 및 commit Test

--- lib/src/main/kotlin/com/santaclose/lib/entity/restaurant/Restaurant.kt
+++ lib/src/main/kotlin/com/santaclose/lib/entity/restaurant/Restaurant.kt
@@ -4,10 +4,12 @@ import com.santaclose.lib.converter.StringListConverter
 import com.santaclose.lib.entity.BaseEntity
 import com.santaclose.lib.entity.appUser.AppUser
 import com.santaclose.lib.entity.location.Location
+import com.santaclose.lib.entity.mountain.Mountain
 import javax.persistence.Column
 import javax.persistence.Convert
 import javax.persistence.Entity
 import javax.persistence.FetchType.LAZY
+import javax.persistence.JoinColumn
 import javax.persistence.ManyToOne
 import javax.persistence.OneToOne
 import javax.validation.constraints.NotNull
@@ -18,6 +20,7 @@ class Restaurant(
   @Column(length = 100) var description: String,
   @Convert(converter = StringListConverter::class)
   var images: MutableList<String> = mutableListOf(),
+  @OneToOne(fetch = LAZY) @JoinColumn(name = "mountain_id") var mountain: Mountain,
   @ManyToOne(fetch = LAZY) @field:NotNull var appUser: AppUser,
   @OneToOne(fetch = LAZY) var location: Location,
 ) : BaseEntity()
