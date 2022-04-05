b3ac15ba8df0f1c3f0652f8bd1a593bdc2d78703
201402407 <banner4@naver.com>
2022-04-06 00:04:13 +0900

    [#62] 맛집 등록 API - Entity mountain 추가 및 commit Test

--- app/src/test/kotlin/com/santaclose/app/util/EntityManagerExtension.kt
+++ app/src/test/kotlin/com/santaclose/app/util/EntityManagerExtension.kt
@@ -14,7 +14,14 @@ fun EntityManager.createAppUser(
 fun EntityManager.createAppRestaurant(
   appUser: AppUser,
   restaurant: Restaurant =
-    Restaurant("name", "description", mutableListOf(), appUser, createAppLocation())
+    Restaurant(
+      "name",
+      "description",
+      mutableListOf(),
+      Mountain("mountainName", "mountainDetail", appUser, createAppLocation()),
+      appUser,
+      createAppLocation()
+    )
 ) = restaurant.also { this.persist(it) }
 
 fun EntityManager.createAppMountain(
