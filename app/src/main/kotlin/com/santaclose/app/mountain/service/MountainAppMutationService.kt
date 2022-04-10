ad61edeb76031fe4f09d20bcfeda643db5837ce1
minwoo <haruLife@inflearn.com>
2022-04-10 17:48:49 +0900

    [#66] 산 등록 input dto 생성

--- app/src/main/kotlin/com/santaclose/app/mountain/service/MountainAppMutationService.kt
+++ app/src/main/kotlin/com/santaclose/app/mountain/service/MountainAppMutationService.kt
@@ -1,3 +1,27 @@
 package com.santaclose.app.mountain.service
 
-class MountainAppMutationService {}
+import com.santaclose.app.mountain.resolver.dto.CreateMountainAppInput
+import com.santaclose.lib.entity.appUser.AppUser
+import com.santaclose.lib.entity.location.Location
+import com.santaclose.lib.entity.mountain.Mountain
+import javax.persistence.EntityManager
+
+class MountainAppMutationService(
+  private val em: EntityManager,
+) {
+
+  fun create(input: CreateMountainAppInput, userId: Long) {
+
+    val mountain =
+      Mountain(
+        name = input.name,
+        images = input.images.toMutableList(),
+        management = input.management,
+        altitude = input.altitude,
+        appUser = em.getReference(AppUser::class.java, userId),
+        location = Location.createPoint(input.longitude.toDouble(), input.latitude.toDouble()),
+      )
+
+    em.persist(mountain)
+  }
+}
