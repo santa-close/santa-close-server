e9e52b4e83981ffab799e9837b5f37a57d82e2ef
201402407 <banner4@naver.com>
2022-04-12 16:12:41 +0900

    [#62] Entity의location 중 주소 nullable 변경

--- lib/src/main/kotlin/com/santaclose/lib/entity/location/Location.kt
+++ lib/src/main/kotlin/com/santaclose/lib/entity/location/Location.kt
@@ -8,11 +8,8 @@ import org.locationtech.jts.geom.GeometryFactory
 import org.locationtech.jts.geom.Point
 
 @Entity
-class Location(
-  @field:NotNull var point: Point,
-  var address: String,
-  @field:NotNull var postcode: String
-) : BaseEntity() {
+class Location(@field:NotNull var point: Point, var address: String, var postcode: String) :
+  BaseEntity() {
   companion object {
     private val factory = GeometryFactory()
 
