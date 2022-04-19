9d1799a9b0d2e169a0cd6e895f0c5aa04cd97bbe
Charming <loadkrnis@gmail.com>
2022-04-19 22:21:09 +0900

    [#71] 산 리뷰 평균 점수 queryRepository

--- app/src/main/kotlin/com/santaclose/app/mountainReview/repository/MountainReviewAppQueryRepositoryImpl.kt
+++ app/src/main/kotlin/com/santaclose/app/mountainReview/repository/MountainReviewAppQueryRepositoryImpl.kt
@@ -1,11 +1,15 @@
 package com.santaclose.app.mountainReview.repository
 
 import com.linecorp.kotlinjdsl.query.spec.expression.EntitySpec
+import com.linecorp.kotlinjdsl.querydsl.expression.avg
 import com.linecorp.kotlinjdsl.querydsl.expression.col
 import com.linecorp.kotlinjdsl.querydsl.from.join
 import com.linecorp.kotlinjdsl.spring.data.SpringDataQueryFactory
 import com.linecorp.kotlinjdsl.spring.data.listQuery
+import com.linecorp.kotlinjdsl.spring.data.selectQuery
+import com.santaclose.app.mountainReview.repository.dto.MountainRatingAverageDto
 import com.santaclose.lib.entity.mountain.Mountain
+import com.santaclose.lib.entity.mountainReview.MountainRating
 import com.santaclose.lib.entity.mountainReview.MountainReview
 import javax.persistence.criteria.JoinType
 import org.springframework.stereotype.Repository
@@ -25,4 +29,24 @@ class MountainReviewAppQueryRepositoryImpl(
       orderBy(col(Mountain::id).desc())
       limit(limit)
     }
+
+  override fun findMountainRatingAverages(mountainId: Long): MountainRatingAverageDto {
+    val selectQuery =
+      springDataQueryFactory.selectQuery<MountainRatingAverageDto> {
+        val mountainReview: EntitySpec<MountainReview> = entity(MountainReview::class)
+        selectMulti(
+          avg(MountainRating::scenery),
+          avg(MountainRating::tree),
+          avg(MountainRating::trail),
+          avg(MountainRating::parking),
+          avg(MountainRating::toilet),
+          avg(MountainRating::traffic),
+        )
+        associate(MountainReview::class, MountainRating::class, on(MountainReview::rating))
+        from(mountainReview)
+        join(MountainReview::mountain, JoinType.INNER)
+        where(col(Mountain::id).equal(mountainId))
+      }
+    return selectQuery.singleResult
+  }
 }
