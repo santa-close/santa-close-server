cc53da8249cf9912123c73992c2671da8bd213bd
201402407 <banner4@naver.com>
2022-05-15 19:47:48 +0900

    [#84] feat: resolver 테스트 코드 작성

--- app/src/test/kotlin/com/santaclose/app/restaurant/resolver/RestaurantAppMutationResolverTest.kt
+++ app/src/test/kotlin/com/santaclose/app/restaurant/resolver/RestaurantAppMutationResolverTest.kt
@@ -3,6 +3,11 @@ package com.santaclose.app.restaurant.resolver
 import com.ninjasquad.springmockk.MockkBean
 import com.santaclose.app.restaurant.service.RestaurantAppMutationService
 import com.santaclose.app.util.AppContextMocker
+import com.santaclose.app.util.GraphqlBody
+import com.santaclose.app.util.gqlRequest
+import com.santaclose.app.util.withSuccess
+import com.santaclose.lib.entity.appUser.type.AppUserRole
+import io.mockk.justRun
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -21,6 +26,34 @@ constructor(
 
   @Nested
   inner class Register {
-    @Test fun `정상적으로 사용자가 식당을 등록한다 - 성공`() {}
+    @Test
+    fun `정상적으로 사용자가 식당을 등록한다 - 성공`() {
+      // given
+      val query =
+        GraphqlBody(
+          """mutation {
+            |  createRestaurant(input: { 
+            |    mountainId: "1"
+            |    name: "식당 이름"
+            |    description: "식당 설명"
+            |    images: ["image1", "image2", "image3"]
+            |    foodTypes: [ASIA, KOREAN, PUB]
+            |    longitude: 100.00
+            |    latitude: 60.00
+            |    address: "주소명"
+            |    postcode: "우편번호"
+            |  })
+            |}
+            """.trimMargin()
+        )
+      justRun { restaurantAppMutationService.createRestaurant(any(), any()) }
+      withMockUser(AppUserRole.USER)
+
+      // when
+      val response = webTestClient.gqlRequest(query)
+
+      // then
+      response.withSuccess("createRestaurant")
+    }
   }
 }
