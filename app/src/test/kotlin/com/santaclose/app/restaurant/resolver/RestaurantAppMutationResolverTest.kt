2faf24c6f4d79a896e25018a86c336f7451c8064
201402407 <banner4@naver.com>
2022-05-15 19:02:25 +0900

    [#84] refactor: ktfmt formatting

--- app/src/test/kotlin/com/santaclose/app/restaurant/resolver/RestaurantAppMutationResolverTest.kt
+++ app/src/test/kotlin/com/santaclose/app/restaurant/resolver/RestaurantAppMutationResolverTest.kt
@@ -3,6 +3,8 @@ package com.santaclose.app.restaurant.resolver
 import com.ninjasquad.springmockk.MockkBean
 import com.santaclose.app.restaurant.service.RestaurantAppMutationService
 import com.santaclose.app.util.AppContextMocker
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
 import org.springframework.boot.test.context.SpringBootTest
@@ -16,5 +18,9 @@ constructor(
   private val webTestClient: WebTestClient,
   @MockkBean private val restaurantAppMutationService: RestaurantAppMutationService
 ) : AppContextMocker() {
-  // TODO: 테스트코드 작성
+
+  @Nested
+  inner class Register {
+    @Test fun `정상적으로 사용자가 식당을 등록한다 - 성공`() {}
+  }
 }
