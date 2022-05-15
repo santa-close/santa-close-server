10a5ce78e8f680412cea1844c0363fee82cd8ee1
201402407 <banner4@naver.com>
2022-05-15 18:43:16 +0900

    [#84] feat: DTO 테스트코드 작성

--- app/src/test/kotlin/com/santaclose/app/restaurant/resolver/dto/CreateRestaurantAppInputTest.kt
+++ app/src/test/kotlin/com/santaclose/app/restaurant/resolver/dto/CreateRestaurantAppInputTest.kt
@@ -4,59 +4,141 @@ import com.expediagroup.graphql.generator.scalars.ID
 import com.santaclose.lib.entity.restaurant.type.FoodType
 import io.kotest.matchers.collections.shouldHaveSize
 import io.kotest.matchers.shouldBe
-import javax.validation.ConstraintViolation
 import javax.validation.Validation
 import org.junit.jupiter.api.Test
 
 internal class CreateRestaurantAppInputTest {
   private val validator = Validation.buildDefaultValidatorFactory().validator
-
-  //  @Test
-  //  fun `위도 및 경도 좌표가 존재하지 않는 좌표값인 경우 오류가 발생한다 - 실패`() {
-  //    // given
-  //    val dto =
-  //      CreateRestaurantAppInput(
-  //        mountainId = ID("1"),
-  //        name = "식당 이름",
-  //        description = "식당 설명",
-  //        images = mutableListOf("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12",
-  // "13"),
-  //        foodTypes = mutableListOf(FoodType.AMERICAN, FoodType.FOOD_COURT, FoodType.ASIA),
-  //        longitude = 120.01,
-  //        latitude = 60.01,
-  //        address = "서울시 성동구 왕십리로 83-21",
-  //        postcode = "04769"
-  //      )
-  //
-  //    // when
-  //    val result = input.toPolygon()
-  //
-  //    // then
-  //    result.equals(rectangle) shouldBe true
-  //  }
+  private val mountainId = ID("1")
+  private val name = "식당 이름"
+  private val description = "식당 설명"
+  private val images = mutableListOf("1", "2", "3", "4", "5", "6", "7", "8", "9", "10")
+  private val foodTypes = mutableListOf(FoodType.AMERICAN, FoodType.FOOD_COURT)
+  private val longitude = 120.01
+  private val latitude = 60.01
+  private val address = "서울시 성동구 왕십리로 83-21"
+  private val postcode = "04769"
 
   @Test
-  fun `음식점 사진의 개수가 10개를 초과한 경우 오류가 발생한다 - 실패`() {
+  fun `모든 조건에 알맞는 값을 입력받는다 - 성공`() {
     // given
     val dto =
       CreateRestaurantAppInput(
-        mountainId = ID("1"),
-        name = "식당 이름",
-        description = "식당 설명",
-        images = mutableListOf("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"),
-        foodTypes = mutableListOf(FoodType.AMERICAN, FoodType.FOOD_COURT, FoodType.ASIA),
-        longitude = 120.01,
-        latitude = 60.01,
-        address = "서울시 성동구 왕십리로 83-21",
-        postcode = "04769"
+        mountainId = mountainId,
+        name = name,
+        description = description,
+        images = images,
+        foodTypes = foodTypes,
+        longitude = longitude,
+        latitude = latitude,
+        address = address,
+        postcode = postcode
       )
 
     // when
     // Valid annotation 을 통과한 경우, violations 의 값은 0이고, 실패한 경우 1이다.
-    val violations: Set<ConstraintViolation<CreateRestaurantAppInput>> = validator.validate(dto)
+    val violations = validator.validate(dto)
+
+    // then
+    violations shouldHaveSize 0
+  }
+
+  @Test
+  fun `사진이 없어도 식당을 등록할 수 있다 - 성공`() {
+    // given
+    val dto =
+      CreateRestaurantAppInput(
+        mountainId = mountainId,
+        name = name,
+        description = description,
+        images = emptyList(),
+        foodTypes = foodTypes,
+        longitude = longitude,
+        latitude = latitude,
+        address = address,
+        postcode = postcode
+      )
+
+    // when
+    val violations = validator.validate(dto)
+
+    // then
+    violations shouldHaveSize 0
+  }
+
+  @Test
+  fun `음식점 사진의 개수가 10개를 초과한 경우 오류가 발생한다 - 실패`() {
+    val overSizeImages =
+      mutableListOf("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12")
+
+    // given
+    val dto =
+      CreateRestaurantAppInput(
+        mountainId = mountainId,
+        name = name,
+        description = description,
+        images = overSizeImages,
+        foodTypes = foodTypes,
+        longitude = longitude,
+        latitude = latitude,
+        address = address,
+        postcode = postcode
+      )
+
+    // when
+    val violations = validator.validate(dto)
 
     // then
     violations shouldHaveSize 1
+    violations.map { it.propertyPath.toString() shouldBe "images" }
+  }
+
+  @Test
+  fun `주소명이 공백이거나 없는 경우 오류가 발생한다 - 실패`() {
+    // given
+    val dto =
+      CreateRestaurantAppInput(
+        mountainId = mountainId,
+        name = name,
+        description = description,
+        images = images,
+        foodTypes = foodTypes,
+        longitude = longitude,
+        latitude = latitude,
+        address = "",
+        postcode = postcode
+      )
+
+    // when
+    val violations = validator.validate(dto)
+
+    // then
+    violations shouldHaveSize 1
+    violations.map { it.propertyPath.toString() shouldBe "address" }
+  }
+
+  @Test
+  fun `우편번호가 공백이거나 없는 경우 오류가 발생한다 - 실패`() {
+    // given
+    val dto =
+      CreateRestaurantAppInput(
+        mountainId = mountainId,
+        name = name,
+        description = description,
+        images = images,
+        foodTypes = foodTypes,
+        longitude = longitude,
+        latitude = latitude,
+        address = address,
+        postcode = ""
+      )
+
+    // when
+    val violations = validator.validate(dto)
+
+    // then
+    violations shouldHaveSize 1
+    violations.map { it.propertyPath.toString() shouldBe "postcode" }
   }
 
   @Test
@@ -64,26 +146,26 @@ internal class CreateRestaurantAppInputTest {
     // given
     val dto =
       CreateRestaurantAppInput(
-        mountainId = ID("1"),
-        name = "식당 이름",
-        description = "식당 설명",
-        images = mutableListOf("1", "2", "3", "4", "5", "6", "7", "8", "9", "10"),
+        mountainId = mountainId,
+        name = name,
+        description = description,
+        images = images,
         foodTypes = emptyList(),
-        longitude = 120.01,
-        latitude = 60.01,
-        address = "서울시 성동구 왕십리로 83-21",
-        postcode = "04769"
+        longitude = longitude,
+        latitude = latitude,
+        address = address,
+        postcode = postcode
       )
 
     // when
-    // Valid annotation 을 통과한 경우, violations 의 값은 0이고, 실패한 경우 1이다.
-    val violations: Set<ConstraintViolation<CreateRestaurantAppInput>> = validator.validate(dto)
+    val violations = validator.validate(dto)
 
     // then
-    // FIXME: 세부적인 오류 메세지 유형 검증까지? 아니면 shouldHaveSize 1만?
+    // FIXME: 세부적인 오류 메세지 유형 검증까지? 아니면 shouldHaveSize 1만? 아니면 검증 실패한 변수명 비교만?
     violations shouldHaveSize 1
     violations.forEach {
       it.messageTemplate shouldBe "{javax.validation.constraints.NotEmpty.message}"
     }
+    violations.map { it.propertyPath.toString() shouldBe "foodTypes" }
   }
 }
