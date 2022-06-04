ce1b8c5ec05683a72c0ea2c26a536d2ed6f8a235
Jake Son <sonjeabin@gmail.com>
2022-06-04 20:33:45 +0900

    [#92] kotest 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,11 +44,12 @@ subprojects {
     }
     testImplementation("io.projectreactor:reactor-test")
     testImplementation("io.mockk:mockk:1.12.3")
-    testImplementation("io.kotest:kotest-assertions-core:5.2.3")
+    testImplementation("io.kotest:kotest-runner-junit5:5.3.0")
     testImplementation("io.kotest.extensions:kotest-assertions-arrow:1.2.5")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.1")
     testImplementation("com.ninja-squad:springmockk:3.1.1")
     testImplementation("com.navercorp.fixturemonkey:fixture-monkey-kotlin:0.3.3")
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.1")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test")
   }
 
   tasks.withType<KotlinCompile> {
