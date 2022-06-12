139f3be5a703c0bb74e35d8bf03db7a1074a8738
Jake Son <sonjeabin@gmail.com>
2022-06-12 19:09:39 +0900

    [#94] cors 설정 파일분리

--- app/src/main/kotlin/com/santaclose/app/AppApplication.kt
+++ app/src/main/kotlin/com/santaclose/app/AppApplication.kt
@@ -4,40 +4,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
-import org.springframework.http.HttpMethod
-import org.springframework.web.cors.CorsConfiguration
-import org.springframework.web.cors.reactive.CorsWebFilter
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource
 
 @EnableJpaAuditing
 @ConfigurationPropertiesScan
 @EntityScan(basePackages = ["com.santaclose.lib.entity"])
 @SpringBootApplication
-class AppApplication {
-//    @Bean
-//    fun wiringFactory() =
-//        KotlinDirectiveWiringFactory(manualWiring = mapOf("auth" to AuthSchemaDirectiveWiring()))
-//
-//    @Bean
-//    fun hooks(wiringFactory: KotlinDirectiveWiringFactory) = CustomSchemaGeneratorHooks(wiringFactory)
-
-    @Bean
-    fun corsFilter(): CorsWebFilter {
-        val config =
-            CorsConfiguration().apply {
-                allowCredentials = true
-                addAllowedOrigin("https://studio.apollographql.com")
-                addAllowedHeader("*")
-                addAllowedMethod(HttpMethod.POST.name)
-            }
-
-        return UrlBasedCorsConfigurationSource()
-            .apply { registerCorsConfiguration("/graphql/**", config) }
-            .let(::CorsWebFilter)
-    }
-}
+class AppApplication
 
 fun main(args: Array<String>) {
     runApplication<AppApplication>(*args)
