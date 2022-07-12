1205662dd870b987e68ec980b1eb48b33f03a19d
Jake Son <sonjeabin@gmail.com>
2022-07-12 23:01:13 +0900

    [#76] 산에 연관된 식당 가져오기 쿼리구현

--- server-app/src/main/kotlin/com/santaclose/app/mountainRestaurant/repository/MountainRestaurantAppQueryRepository.kt
+++ server-app/src/main/kotlin/com/santaclose/app/mountainRestaurant/repository/MountainRestaurantAppQueryRepository.kt
@@ -1,7 +1,10 @@
 package com.santaclose.app.mountainRestaurant.repository
 
 import com.santaclose.app.mountainRestaurant.repository.dto.LatestMountainDto
+import com.santaclose.app.mountainRestaurant.repository.dto.LatestRestaurantDto
 
 interface MountainRestaurantAppQueryRepository {
     fun findMountainByRestaurant(id: Long, limit: Int): List<LatestMountainDto>
+
+    fun findRestaurantByMountain(id: Long, limit: Int): List<LatestRestaurantDto>
 }
