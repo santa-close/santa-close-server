2e90eedce834c6169c937ae01a61fd0447d0ed1a
Jake Son <sonjeabin@gmail.com>
2022-07-30 21:18:53 +0900

    [#98] 의존성 업데이트

--- server-lib/src/main/kotlin/com/santaclose/lib/s3Upload/S3Uploader.kt
+++ server-lib/src/main/kotlin/com/santaclose/lib/s3Upload/S3Uploader.kt
@@ -5,6 +5,7 @@ import aws.sdk.kotlin.runtime.auth.credentials.StaticCredentialsProvider
 import aws.sdk.kotlin.runtime.endpoint.AwsEndpoint
 import aws.sdk.kotlin.runtime.endpoint.StaticEndpointResolver
 import aws.sdk.kotlin.services.s3.S3Client
+import aws.sdk.kotlin.services.s3.putObject
 import aws.smithy.kotlin.runtime.auth.awscredentials.Credentials
 import aws.smithy.kotlin.runtime.content.ByteStream
 
