b01b14d1bd0f983e3b19d62ac02e0242d79b326e
Jake Son <sonjeabin@gmail.com>
2022-08-07 07:30:41 +0900

    [#102] WebClient 를 okhttp 로 변경

--- server-lib/src/main/kotlin/com/santaclose/lib/auth/kakao/KakaoAuth.kt
+++ server-lib/src/main/kotlin/com/santaclose/lib/auth/kakao/KakaoAuth.kt
@@ -3,56 +3,67 @@ package com.santaclose.lib.auth.kakao
 import arrow.core.Either
 import arrow.core.Either.Companion.catch
 import arrow.core.continuations.either
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.santaclose.lib.auth.Profile
-import com.santaclose.lib.logger.logger
-import io.netty.channel.ChannelOption
+import okhttp3.FormBody
+import okhttp3.OkHttpClient
+import okhttp3.Request
 import org.springframework.http.HttpHeaders
-import org.springframework.http.client.reactive.ReactorClientHttpConnector
-import org.springframework.web.reactive.function.BodyInserters.fromFormData
-import org.springframework.web.reactive.function.client.WebClient
-import org.springframework.web.reactive.function.client.awaitBody
-import reactor.netty.http.client.HttpClient
-import java.time.Duration
 
 class KakaoAuth(
-    private val builder: WebClient.Builder,
     private val clientId: String,
     private val redirectUri: String,
     private val tokenUri: String = "https://kauth.kakao.com/oauth/token",
     private val userUri: String = "https://kapi.kakao.com/v2/user/me",
 ) {
-    private val client: WebClient =
-        HttpClient.create()
-            .responseTimeout(Duration.ofSeconds(3))
-            .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 3000)
-            .let(::ReactorClientHttpConnector)
-            .let { builder.clientConnector(it).build() }
-    private val logger = logger()
+    private val okhttp = OkHttpClient()
+    private val objectMapper = jacksonObjectMapper()
+        .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
 
-    suspend fun getProfile(code: String): Either<Throwable, Profile> =
-        either<Throwable, Profile> {
+    fun getProfile(code: String): Either<Throwable, Profile> =
+        either.eager {
             val tokenResponse = getAccessToken(code).bind()
             val userResponse = getUser(tokenResponse.accessToken).bind()
 
             userResponse.toProfile()
         }
-            .tapLeft { logger.error(it.message, it) }
 
-    suspend fun getAccessToken(code: String): Either<Throwable, KakaoTokenResponse> = catch {
-        client
-            .post()
-            .uri(tokenUri)
-            .body(
-                fromFormData("grant_type", "authorization_code")
-                    .with("client_id", clientId)
-                    .with("redirect_uri", redirectUri)
-                    .with("code", code),
+    fun getAccessToken(code: String): Either<Throwable, KakaoTokenResponse> = catch {
+        val request = Request
+            .Builder()
+            .url(tokenUri)
+            .post(
+                FormBody
+                    .Builder()
+                    .addEncoded("grant_type", "authorization_code")
+                    .addEncoded("client_id", clientId)
+                    .addEncoded("redirect_uri", redirectUri)
+                    .addEncoded("code", code)
+                    .build(),
             )
-            .retrieve()
-            .awaitBody()
+            .build()
+        val response = okhttp.newCall(request).execute()
+
+        if (response.isSuccessful) {
+            objectMapper.readValue(response.body?.string(), KakaoTokenResponse::class.java)
+        } else {
+            throw Exception("${response.message} ${response.body?.string()}")
+        }
     }
 
-    suspend fun getUser(token: String): Either<Throwable, KakaoUserResponse> = catch {
-        client.get().uri(userUri).header(HttpHeaders.AUTHORIZATION, "Bearer $token").retrieve().awaitBody()
+    fun getUser(token: String): Either<Throwable, KakaoUserResponse> = catch {
+        val request = Request
+            .Builder()
+            .url(userUri)
+            .header(HttpHeaders.AUTHORIZATION, "Bearer $token")
+            .build()
+        val response = okhttp.newCall(request).execute()
+
+        if (response.isSuccessful) {
+            objectMapper.readValue(response.body?.string(), KakaoUserResponse::class.java)
+        } else {
+            throw Exception("${response.message} ${response.body?.string()}")
+        }
     }
 }
