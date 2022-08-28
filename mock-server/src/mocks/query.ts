897071acf52c28d6abf31d3ece42986c3f9c7c76
Jake Son <sonjeabin@gmail.com>
2022-08-28 18:13:54 +0900

    [#113] npm 패키지 업데이트

--- mock-server/src/mocks/query.ts
+++ mock-server/src/mocks/query.ts
@@ -1,11 +1,11 @@
-import faker from '@faker-js/faker';
+import { faker } from '@faker-js/faker';
 import { readFileSync } from 'fs';
 import { join } from 'path';
 
 export const sample = () => ({
   name: faker.name.findName(),
   price: faker.datatype.number({ min: 0, max: 1000 }),
-  status: faker.random.arrayElement(['OPEN', 'CLOSE']),
+  status: faker.helpers.arrayElement(['OPEN', 'CLOSE']),
 });
 
 export const categories = () =>
