55b0835aadb7cf0cf1b21c391c83a8f493ce7b7d
Jake Son <sonjeabin@gmail.com>
2023-03-19 14:00:51 +0900

    [#146] okhttp3 라이브러리 제거

--- server-app/src/main/kotlin/com/santaclose/app/auth/service/AuthManager.kt
+++ server-app/src/main/kotlin/com/santaclose/app/auth/service/AuthManager.kt
@@ -1,6 +1,7 @@
 package com.santaclose.app.auth.service
 
 import com.santaclose.app.config.KakaoConfig
+import com.santaclose.lib.auth.kakao.KakaoApiService
 import com.santaclose.lib.auth.kakao.KakaoAuth
 import com.santaclose.lib.auth.kakao.KakaoAuthService
 import org.springframework.stereotype.Service
@@ -8,10 +9,12 @@ import org.springframework.stereotype.Service
 @Service
 class AuthManager(
     kakaoAuthService: KakaoAuthService,
+    kakaoApiService: KakaoApiService,
     kakaoConfig: KakaoConfig,
 ) {
     private val kakaoAuth = KakaoAuth(
         kakaoAuthService,
+        kakaoApiService,
         kakaoConfig.clientId,
         kakaoConfig.redirectUri,
     )
