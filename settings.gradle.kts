e3faaae67dedf527d6e98eeb4a87359dac0fd753
Jake Son <sonjeabin@gmail.com>
2023-04-09 08:10:58 +0900

    [#153] version catalog 파일 위치변경

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,4 @@
 rootProject.name = "santa-close-server"
 
-dependencyResolutionManagement {
-    versionCatalogs {
-        create("libs") {
-            from(files("libs.versions.toml"))
-        }
-    }
-}
-
 include(":server-lib")
 include(":server-app")
